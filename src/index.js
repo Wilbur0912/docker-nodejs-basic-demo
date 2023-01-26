b9a376824c86346c458fec977201768688d53e44
Wilbur0912 <89004015+Wilbur0912@users.noreply.github.com>
2023-01-26 13:15:22 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 
 app.get('/', (req, res ) => 
-    res.json({ message: 'docker is easy 🐳' }) 
+    res.json({ message: 'docker iss easy 🐳' }) 
 );
 
 const port = process.env.PORT || 8080;
